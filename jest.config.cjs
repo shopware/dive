6c50a67c320d68ed6478e4ce1c531ab6eaaa4855
Felix Frank <felix.m.frank@outlook.de>
2024-05-11 16:43:32 +0200

    Added coverage threshold.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,12 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
+    coverageThreshold: {
+        global: {
+          branches: 90,
+          functions: 80,
+          lines: 90,
+          statements: 80,
+        },
+    },
 };
