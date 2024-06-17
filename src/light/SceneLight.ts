6343f5fa3d5d7bb79e924b39b0986f20a715a7b2
Felix Frank <felix.m.frank@outlook.de>
2024-06-17 08:35:40 +0200

    Changed inheritance logic for lights.

--- src/light/SceneLight.ts
+++ src/light/SceneLight.ts
@@ -11,50 +11,51 @@ import { Color, DirectionalLight, HemisphereLight, Object3D } from "three";
 
 export default class DIVESceneLight extends Object3D {
 
-    private hemiLight: HemisphereLight;
-    private dirLight: DirectionalLight;
+    private _hemiLight: HemisphereLight;
+    private _dirLight: DirectionalLight;
 
     constructor() {
         super();
 
-        this.name = "SceneLight";
+        this.name = 'DIVESceneLight';
 
-        this.hemiLight = new HemisphereLight(0xffffff, 0xffffff, 2);
-        this.hemiLight.layers.mask = PRODUCT_LAYER_MASK;
-        this.hemiLight.position.set(0, 50, 0);
-        this.add(this.hemiLight);
+        this._hemiLight = new HemisphereLight(0xffffff, 0xffffff, 2);
+        this._hemiLight.layers.mask = PRODUCT_LAYER_MASK;
+        this._hemiLight.position.set(0, 50, 0);
+        this.add(this._hemiLight);
 
-        this.dirLight = new DirectionalLight(0xffffff, 3);
-        this.dirLight.layers.mask = PRODUCT_LAYER_MASK;
-        this.dirLight.position.set(1, 1.75, 1);
-        this.dirLight.position.multiplyScalar(30);
-        this.dirLight.castShadow = true;
+        this._dirLight = new DirectionalLight(0xffffff, 3);
+        this._dirLight.layers.mask = PRODUCT_LAYER_MASK;
+        this._dirLight.position.set(1, 1.75, 1);
+        this._dirLight.position.multiplyScalar(30);
+        this._dirLight.castShadow = true;
 
-        this.dirLight.shadow.mapSize.width = 2048;
-        this.dirLight.shadow.mapSize.height = 2048;
+        this._dirLight.shadow.mapSize.width = 2048;
+        this._dirLight.shadow.mapSize.height = 2048;
 
         const d = 5;
 
-        this.dirLight.shadow.camera.left = - d;
-        this.dirLight.shadow.camera.right = d;
-        this.dirLight.shadow.camera.top = d;
-        this.dirLight.shadow.camera.bottom = - d;
+        this._dirLight.shadow.camera.left = - d;
+        this._dirLight.shadow.camera.right = d;
+        this._dirLight.shadow.camera.top = d;
+        this._dirLight.shadow.camera.bottom = - d;
 
-        this.dirLight.shadow.camera.far = 3500;
-        this.add(this.dirLight);
+        this._dirLight.shadow.camera.far = 3500;
+        this.add(this._dirLight);
     }
 
     public SetColor(color: Color): void {
-        this.hemiLight.color = color;
-        this.dirLight.color = color;
+        this._hemiLight.color = color;
+        this._dirLight.color = color;
     }
 
     public SetIntensity(intensity: number): void {
-        this.hemiLight.intensity = intensity * 2;
-        this.dirLight.intensity = intensity * 3;
+        this._hemiLight.intensity = intensity * 2;
+        this._dirLight.intensity = intensity * 3;
     }
 
     public SetEnabled(enabled: boolean): void {
-        this.visible = enabled;
+        this._hemiLight.visible = enabled;
+        this._dirLight.visible = enabled;
     }
 }
