e8366b1b87edd42bac399144097469318b45afea
Felix Frank <felix.m.frank@outlook.de>
2024-09-11 09:52:04 +0200

    Added export.

--- src/com/index.ts
+++ src/com/index.ts
@@ -1,4 +1,4 @@
 import DIVECommunication from './Communication.ts';
 export type { Actions } from './actions/index.ts';
-export type { COMLight, COMModel, COMPov, COMEntity } from './types.ts';
+export type { COMPrimitive, COMLight, COMModel, COMPov, COMEntity } from './types.ts';
 export default DIVECommunication;
