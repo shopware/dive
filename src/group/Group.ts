a81865cdefc511744c000053fad740cf01185486
Felix Frank <felix.m.frank@outlook.de>
2024-09-18 14:07:12 +0200

    Created basic Group class.

---
+++ src/group/Group.ts
@@ -0,0 +1,12 @@
+import { Object3D } from "three";
+import { DIVEMoveable } from "../interface/Moveable";
+import { DIVESelectable } from "../interface/Selectable";
+
+export class DIVEGroup extends Object3D implements DIVESelectable, DIVEMoveable {
+    public isSelectable: true = true;
+    public isMoveable: true = true;
+
+    constructor() {
+        super();
+    }
+}
