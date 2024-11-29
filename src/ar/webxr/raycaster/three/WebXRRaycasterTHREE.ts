d46b63a6e1d29d4c21d59bdcdc76076ecb6a00e5
Felix Frank <felix.m.frank@outlook.de>
2024-11-29 15:11:29 +0100

    Fixed scene raycast.

--- src/ar/webxr/raycaster/three/WebXRRaycasterTHREE.ts
+++ src/ar/webxr/raycaster/three/WebXRRaycasterTHREE.ts
@@ -1,4 +1,4 @@
-import { Intersection, type Mesh, Raycaster, XRTargetRaySpace } from "three";
+import { type Intersection, type Mesh, Raycaster, type XRTargetRaySpace } from "three";
 import { type DIVERenderer } from "../../../../renderer/Renderer";
 import { type DIVEScene } from "../../../../scene/Scene";
 import { type DIVEHitResult } from "../WebXRRaycaster";
@@ -7,7 +7,7 @@ export class DIVEWebXRRaycasterTHREE {
     private _renderer: DIVERenderer;
     private _scene: DIVEScene;
 
-    private _controller: XRTargetRaySpace | undefined;
+    private _controller: XRTargetRaySpace;
 
     // internal raycaster
     private _raycaster: Raycaster = new Raycaster();
@@ -17,35 +17,19 @@ export class DIVEWebXRRaycasterTHREE {
         this._scene = scene;
 
         this._controller = this._renderer.xr.getController(0);
-
-        scene.XRRoot.add(this._controller);
-    }
-
-    public Dispose(): void {
-        // dispose code here
     }
 
     public async Init(): Promise<this> {
-        // init code here
-
         console.log('DIVEWebXRRaycasterTHREE: Initialized');
         return Promise.resolve(this);
     }
 
-    // public Update(frame: XRFrame): void {
-    //     // update code here
-    // }
-
     public GetIntersections(): DIVEHitResult[] {
-        if (!this._controller) return [];
-
         this._controller.updateMatrixWorld();
         this._raycaster.setFromXRController(this._controller);
         const intersections = this._raycaster.intersectObjects(this._scene.XRRoot.XRModelRoot.children);
 
         if (intersections.length === 0) return [];
-
-        console.log(intersections);
 
         return intersections.map((intersection: Intersection) => {
             return {
