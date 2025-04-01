160e92c4cf1ad6c4d66e7237d8341f5881ee4298
Felix Frank <f.frank@shopware.com>
2025-04-01 10:24:51 +0200

    Change to esnext build.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,24 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     build: {
+        target: 'esnext', // Ensure compatibility with modern JS
         rollupOptions: {
             input: 'src/dive.ts',
+            output: [
+                {
+                    format: 'esm',
+                    entryFileNames: '[name].mjs', // Name the output files as .mjs
+                },
+                {
+                    format: 'cjs', // CommonJS format
+                    entryFileNames: '[name].cjs', // Name for .cjs files
+                },
+                {
+                    format: 'iife', // For traditional .js files
+                    entryFileNames: '[name].js', // Name for .js files
+                    name: 'dive'
+                }
+            ]
         },
         lib: {
             entry: 'src/dive.ts',
