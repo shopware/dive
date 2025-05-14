e730193dda006976ea51806284cd9e915e8bc4f9
Felix Frank <f.frank@shopware.com>
2025-05-14 14:14:22 +0200

    Added class exports instead of types.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ export * from './core/index.ts';
 export * from './helpers/index.ts';
 
 // errors
-export type { ARCompatibilityError } from './error/ar-compatibility/ar-compatibility-error.ts';
-export type { FileTypeError } from './error/file-type/file-type-error.ts';
-export type { NetworkError } from './error/network/network-error.ts';
-export type { ParseError } from './error/parse/parse-error.ts';
+export { ARCompatibilityError } from './error/ar-compatibility/ar-compatibility-error.ts';
+export { FileTypeError } from './error/file-type/file-type-error.ts';
+export { NetworkError } from './error/network/network-error.ts';
+export { ParseError } from './error/parse/parse-error.ts';
 
 // types
 export * from './types/index.ts';
