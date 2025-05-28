84a511772186c99d84b1265a72b3bbaa54e2de0e
Felix Frank <f.frank@shopware.com>
2025-05-28 17:09:23 +0200

    Expose engine and interface types.

---
+++ src/engine/index.ts
@@ -0,0 +1,6 @@
+export * from './camera/PerspectiveCamera.ts';
+export * from './clock/Clock.ts';
+export * from './renderer/Renderer.ts';
+export * from './resize/ResizeManager.ts';
+export * from './scene/Scene.ts';
+export * from './Engine.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 // Named exports
 export * from './core/index.ts';
+export * from './engine/index.ts';
+export * from './interfaces/index.ts';
 export * from './helpers/index.ts';
 
 // errors
-export { ARCompatibilityError } from './error/ar-compatibility/ar-compatibility-error.ts';
+export {
+    ARError,
+    ARDesktopPlatformError,
+    ARQuickLookNotIOSDeviceError,
+    ARQuickLookNotSafariError,
+    ARQuickLookVersionMismatchError,
+    ARQuickLookUnknownError,
+} from './modules/ar/error/ar-errors.ts';
 export { FileTypeError } from './error/file-type/file-type-error.ts';
 export { NetworkError } from './error/network/network-error.ts';
 export { ParseError } from './error/parse/parse-error.ts';
