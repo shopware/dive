ac384e9103d0ac08b062ef1c1742244052a8415a
Felix Frank <f.frank@shopware.com>
2025-07-09 09:44:49 +0200

    Moved type to correct place.

--- src/plugins/toolbox/index.ts
+++ src/plugins/toolbox/index.ts
@@ -2,3 +2,4 @@ export * from './src/Toolbox.ts';
 export * from './src/BaseTool.ts';
 export * from './src/select/SelectTool.ts';
 export * from './src/transform/TransformTool.ts';
+export * from './types/index.ts';
