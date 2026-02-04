09ede327127b3e20139f9ec5bffae57cc2944720
Felix Frank <f.frank@shopware.com>
2026-02-04 10:16:07 +0100

    chore: add dev mode welcome log.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
-// Declare the global constant injected by Vite
+// Declare the global constants injected by Vite
 // No other code needed
 declare const __APP_VERSION__: string;
+declare const __DEV_MODE__: boolean;
